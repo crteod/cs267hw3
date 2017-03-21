0da6736ec21943ce511d5abf13854e487ae4823f
koosterb <kjoosterbaan@gmail.com>
2017-03-20 20:10:43 -0700

    initial stab at upc

---
+++ kmerHash_upc.h
@@ -0,0 +1,37 @@
+#ifndef KMER_HASH_H
+#define KMER_HASH_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <math.h>
+#include <upc.h>
+
+#include "commonDefaults_upc.h"
+
+/* Creates a hash table and (pre)allocates memory for the memory heap */
+hash_table_t* createHashTable(int64_t nEntries, memory_heap_t *memory_heap) {
+  hash_table_t *result;
+  int64_t n_buckets = nEntries * LOAD_FACTOR;
+  
+  result = (hash_table_t*) upc_alloc(sizeof(hash_table_t));
+  result->size = n_buckets;
+  result->table = (bucket_t*) upc_all_alloc(n_buckets , sizeof(bucket_t)); //need to initialize to 0
+  upc_memset(result->table, 0, n_buckets * sizeof(bucket_t));
+  
+  if (result->table == NULL) {
+    fprintf(stderr, "ERROR: Could not allocate memory for the hash table: %lld buckets of %lu bytes\n", n_buckets, sizeof(bucket_t));
+    fprintf(stderr, "ERROR: Are you sure that your input is of the correct KMER_LENGTH in Makefile?\n");
+    exit(1);
+  }
+  
+  memory_heap->heap = (kmer_t *) upc_alloc(nEntries * sizeof(kmer_t));
+  if (memory_heap->heap == NULL) {
+    fprintf(stderr, "ERROR: Could not allocate memory for the heap!\n");
+    exit(1);
+  }
+  memory_heap->posInHeap = 0;
+  
+  return result;
+}
